2e5d6a53154ef8fb2091336824a6bac12b08cb84
Mohammad Javad Afkari <afkari.1370@gmail.com>
2017-07-08 19:55:51 +0430

    + Add structure

---
+++ src/PayIr.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: wmateam
+ * Date: 7/8/17
+ * Time: 7:52 PM
+ */
+
+namespace wmateam\payIr;
+
+class PayIr
+{
+    public function __construct()
+    {
+    }
+}
---
+++ tests/PayIrTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: wmateam
+ * Date: 7/8/17
+ * Time: 7:52 PM
+ */
+
+use wmateam\payIr\PayIr;
+
+class PayIrTest extends PHPUnit_Framework_TestCase
+{
+    public function __construct()
+    {
+    }
+}
